7f98214a947db837f189f382af906f1b55efc213
LarzlRael <46308154+LarzlRael@users.noreply.github.com>
2021-07-18 12:45:10 -0400

    added best email messages

--- server/src/controllers/mailController.ts
+++ server/src/controllers/mailController.ts
@@ -32,14 +32,16 @@ export const SendEmailActivation = async (req: Request, res: Response) => {
     try {
         const { email } = req.body;
         const token = await generarJWT('', '5M', email);
+        const navigateTo = `${myUrl}/sendmail/verifycheck/${token}`;
+
         transporter.sendMail({
             from: 'N.E.O', // sender address
             to: email, // list of receivers
             subject: "Activacion de email", // Subject line
             /* text: "Hello world?", // plain text body */
-            html: `<p>
-            Para activar tu cuenta ingrese a <a href="${myUrl}/verifycheck/${token}">verificar email</a>
-            </p>`, // html body
+
+            html: htmlTemplate(email, navigateTo), // html body
+
         });
         res.json({
             ok: true,
@@ -162,15 +164,62 @@ export const passwordChanged = async (req: Request, res: Response) => {
 
     } else {
         res.send('Error');
-
     }
 
 
 }
 
-/* getMyHostUrl(req:Request): string{
-const hostname = req.headers.host;
-    const protocol = req.protocol;
+const htmlTemplate = (name: string, navigateTo: string): string => {
 
-    const myUrl = `${protocol}://${hostname}`;
-} */
+    return `
+    <body>
+
+    <div class="container" style="
+    width: 90%;
+    margin: auto;
+    font-family: Arial, Helvetica, sans-serif;
+        -webkit-box-shadow: 1px 1px 26px -4px #1b242d;
+        box-shadow: 1px 1px 26px -4px #1b242d;
+        ">
+        <div class="header" style="border-top: 1px solid #ccc;
+        background: #1b242d;
+        height:150px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        ">
+            <img src="https://negocioexitoso.online/wp-content/uploads/2021/06/logo-blanco-1Kx1K-150x150.png"
+                style="height: 70%;" alt="">
+
+        </div>
+
+        <div class="body" style="padding: 1rem;">
+            <p>${name}</p>
+            <p>Para activar su cuenta en neo, por favor verifica tu email, Tu cuenta no se puede activar hasta que tu
+                direccion
+                de correo electronico este confirmado.</p>
+
+            <div style="display: flex;
+            width: 100%;
+            justify-content: center;
+            margin: 2rem 0;
+            ">
+                <a href="${navigateTo}" 
+                style="
+            text-decoration: none;
+            padding: 1rem 3rem;
+            color: white;
+            background: #1b242d;
+            text-align: center;
+            border: none;
+            font-weight: bold;
+            font-size: 0.9rem;
+            border-radius: 40px;
+            transition: all 0.3s;
+            ">Confimar tu email</a>
+            </div>
+        </div>
+    </div>
+</body>
+    `;
+}
